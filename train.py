94a80d3c11356ae88816c18e7aa77172ffe59752
redstarh <red7368@gmail.com>
2023-11-07 17:21:55 +0900

    added github actions-37 train.py update

--- train.py
+++ train.py
@@ -33,10 +33,10 @@ test_score = regr.score(X_test, y_test) * 100
 
 # Write scores to a file
 with open("metrics.txt", 'w') as outfile:
-        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
-        print("Training variance explained: %2.1f%%\n" % train_score)
-        print("Test variance explained: %2.1f%%\n" % test_score)
+    outfile.write("Training variance explained: %2.1f%%\n" % train_score)
+    outfile.write("Test variance explained: %2.1f%%\n" % test_score)
+    #print("Training variance explained: %2.1f%%\n" % train_score)
+    #print("Test variance explained: %2.1f%%\n" % test_score)
 
 
 ##########################################
